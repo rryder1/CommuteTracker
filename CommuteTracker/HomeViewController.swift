ddeab2cc0ee43c481d3fa14222cb1377d15ae660
Robby Ryder <Rryder1@Roberts-MacBook-Pro.local>
2017-06-18 13:11:39 -0400

    Initial

--- CommuteTracker/HomeViewController.swift
+++ CommuteTracker/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CommuteTracker
 //
 //  Created by Robert Ryder on 6/17/17.
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    @IBOutlet weak var consecutiveCommutes: UILabel!
+    @IBOutlet weak var totalMiles: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ Pods/Target Support Files/Pods-CommuteTracker/Pods-CommuteTracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CommuteTracker : NSObject
+@end
+@implementation PodsDummy_Pods_CommuteTracker
+@end
